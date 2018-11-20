1c7a027d2d49cd8ebf4fd8766fb24e87ee592e0e
Kirill Kamakin <k.kamakin@protonmail.ch>
2018-11-20 16:54:39 +0300

    Initialize termios

---
+++ src/native/term.rs
@@ -0,0 +1,19 @@
+extern crate libc;
+
+use self::libc::{termios, tcgetattr, tcsetattr, c_int};
+
+use std::os::unix::io::RawFd;
+
+use ::{Result, Error};
+
+pub fn setup_tty(fd: RawFd, on: i32) -> Result<()> {
+    let configuration = unsafe { get_attr(fd)? };
+
+    Ok(())
+}
+
+unsafe fn get_attr(fd: RawFd) -> Result<termios> {
+    let mut buf: termios = std::mem::zeroed();
+    let result: c_int = tcgetattr(fd, &mut buf);
+    errno!(result, buf)
+}
